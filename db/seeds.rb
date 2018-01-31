bf892b9f4fcd2570973c63ba306f46336d0f6fbb
Jesus Garlea <chamoysvoice@gmail.com>
2018-01-31 01:03:02 -0600

    Feat: Create demo users rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -204,3 +204,15 @@ puts 'Done polls'
 # end
 
 # puts "Done voters: #{Time.now - t}s"
+
+unless Rails.env == 'production'
+  puts 'Available Users:\n'
+  puts 'Manager: manager@irrational.ly Pass: manager'
+  puts 'Capturist: capturist@irrational.ly Pass: capturist'
+  puts 'Supervisor: supervisor@irrational.ly Pass: supervisor'
+  puts 'Superadmin: superadmin@irrational.ly Pass: superadmin'
+  User.create(email: 'manager@irrational.ly', password: 'manager', manager: true)
+  User.create(email: 'capturist@irrational.ly', password: 'capturist', capturist: true)
+  User.create(email: 'supervisor@irrational.ly', password: 'supervisor', supervisor: true)
+  User.create(email: 'superadmin@irrational.ly', password: 'superadmin', superadmin: true)
+end
