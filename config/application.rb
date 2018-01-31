b5cb9825f2d8fc8cc9bfe2a61d064ec6e64802da
Jesus Garlea <chamoysvoice@gmail.com>
2018-01-31 00:42:27 -0600

    Feat: Add cors policy

--- config/application.rb
+++ config/application.rb
@@ -9,27 +9,17 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ElectionApi
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins Rails.application.secrets.client_host
         resource '*', headers: :any, methods: %i[get post options delete put]
       end
     end
