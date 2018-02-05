666c6ede57ff52aa024938b8176230118bfdb69c
Luis Mario Díaz R <mariodiar@gmail.com>
2018-02-05 00:06:54 -0600

    Using fast_jsonapi with demographics model

--- app/serializers/demographic_serializer.rb
+++ app/serializers/demographic_serializer.rb
@@ -1,6 +1,7 @@
-class DemographicSerializer < ActiveModel::Serializer
+class DemographicSerializer
+  include FastJsonapi::ObjectSerializer
   attributes :id, :section_code, :muni_code, :state_code, :district_code,
              :total, :year, :hombres, :mujeres, :hijos, :entidad_nac,
              :entidad_inm, :entidad_mig, :limitacion, :analfabetismo, :educacion_av,
-             :pea, :no_serv_salud, :matrimonios, :hogares, :hogares_pob, :auto
+             :pea, :no_serv_salud, :matrimonios, :hogares, :hogares_jefa, :hogares_pob, :auto
 end
